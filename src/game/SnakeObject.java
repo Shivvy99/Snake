ca02ada22586a083dc0ec4e29af14c941747aa86
Shyam <shyamganapathy@live.com>
2024-02-26 17:24:00 -0500

    Initial commit

---
+++ src/game/SnakeObject.java
@@ -0,0 +1,22 @@
+package game;
+
+import java.awt.Point.*;
+// 
+public class SnakeObject extends Polygon {
+    final static Point[] snakePoints = {new Point(0, 0), new Point(0, 10),
+            new Point(10, 10), new Point(10, 0)};
+
+    public SnakeObject() {
+        super(snakePoints, new Point(0, 0), 0);
+    }
+
+    public void paint() {
+        Point[] snakePoints = this.getPoints();
+
+        double[] xPoints = new double[snakePoints.length];
+        for (int i = 0; i < xPoints.length; i++) {
+            xPoints[i] = snakePoints[i].x;
+        }
+    }
+}
+
