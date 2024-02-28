801e3f93b22d07c4d57e072af623f19e870a8aba
Shyam <shyamganapathy@live.com>
2024-02-27 19:30:31 -0500

    In person progress 2/27
    checkered background, structure and filled in snake object

--- src/game/Snake.java
+++ src/game/Snake.java
@@ -13,25 +13,49 @@ class Snake extends Game {
 	static int counter = 0;
 	private SnakeObject snake;
 
+
   public Snake() {
-    super("Snake!",800,600);
+    super("Snake!",600,600);
     this.setFocusable(true);
 	this.requestFocus();
   }
   
 	public void paint(Graphics brush) {
-    	brush.setColor(Color.blue);
-    	brush.fillRect(0,0,width,height);
     	
     	// sample code for printing message for debugging
     	// counter is incremented and this message printed
     	// each time the canvas is repainted
+		drawCheckeredBackground(brush);
     	counter++;
-    	brush.setColor(Color.white);
+		brush.setColor(Color.BLUE);
     	brush.drawString("Counter is " + counter,10,10);
 		snake = new SnakeObject();
+		brush.setColor(Color.GREEN);
 		snake.paint(brush);
+
   }
+
+	private void drawCheckeredBackground(Graphics g) {
+		boolean isBlack = false;
+		int width = getWidth();
+		int height = getHeight();
+		int squareSize = 30;
+
+		for (int y = 0; y < height; y += squareSize) {
+			isBlack = !isBlack;
+
+			for (int x = 0; x < width; x += squareSize) {
+				if (isBlack) {
+					g.setColor(Color.black);
+				} else {
+					g.setColor(Color.WHITE);
+				}
+				g.fillRect(x, y, squareSize, squareSize);
+
+				isBlack = !isBlack;
+			}
+		}
+	}
   
 	public static void main (String[] args) {
    		Snake a = new Snake();
--- src/game/SnakeObject.java
+++ src/game/SnakeObject.java
@@ -1,12 +1,20 @@
 package game;
 
 import java.awt.*;
-public class SnakeObject extends Polygon {
-    final static Point[] snakePoints = {new Point(0, 0), new Point(0, 10),
-            new Point(10, 10), new Point(10, 0)};
+import java.awt.event.*;
+
+public class SnakeObject extends Polygon implements KeyListener{
+    final static Point[] spawnPoints = {new Point(0, 0), new Point(0,
+            30),
+            new Point(30, 30), new Point(30, 0)};
+    private Point[] snakePoints;
+    private static boolean north = true, east = false, south = false, west = false;
+
+
 
     public SnakeObject() {
-        super(snakePoints, new Point(0, 0), 0);
+        super(spawnPoints, new Point(300, 300), 0.0);
+        snakePoints = spawnPoints;
     }
 
     public void paint(Graphics brush) {
@@ -21,7 +29,33 @@ public class SnakeObject extends Polygon {
         for (int i = 0; i < yPoints.length; i++) {
             yPoints[i] = (int) snakePoints[i].getY();
         }
+        brush.fillPolygon(xPoints, yPoints, xPoints.length);
+    }
 
-        brush.drawPolygon(xPoints, yPoints, xPoints.length);
+    public void move() {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    public void keyPressed(KeyEvent e) {
+        if (e.equals("VK_d") && !west) {
+            east = true; north = false; south = false;
+        } else if (e.equals("VK_a") && !east) {
+            west = true; north = false; south = false;
+        } else if (e.equals("VK_w") && !south) {
+            north = true; east = false; west = false;
+        } else if (e.equals("VK_s") && !north) {
+            south = true; east = false; west = false;
+        }
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
     }
 }
