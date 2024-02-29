8f28399338b7453249ea14763c48f690cf486985
Shyam <shyamganapathy@live.com>
2024-02-28 22:19:44 -0500

    formatting fixes
    fixed formatting and made spawnpoints final

--- src/game/Snake.java
+++ src/game/Snake.java
@@ -7,84 +7,73 @@ NOTE: This class is the metaphorical "main method" of your program,
       it is your control center.
 
 */
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.Timer;
 
-
 class Snake extends Game {
-	static int counter = 0;
-	private static SnakeObject snake;
-	public int score = 0;
-	private Timer timer;
+    static int counter = 0;
+    private static SnakeObject snake;
+    public int score = 0;
+    private Timer timer;
 
+    public Snake() {
+        super("Snake!", 500, 500);
+        this.setFocusable(true);
+        this.requestFocus();
+        snake = new SnakeObject();
+        this.addKeyListener(snake);
+        timer = new Timer(100, new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                snake.move();
+                repaint();
+            }
+        });
+        timer.start();
+    }
 
+    public void paint(Graphics brush) {
+        drawCheckeredBackground(brush);
+        counter++;
+        brush.setColor(Color.BLUE);
+        brush.drawString("Counter is " + counter, 10, 10);
+        brush.setColor(Color.GREEN);
+        snake.paint(brush);
+    }
 
-	public Snake() {
-    super("Snake!",500,500);
-    this.setFocusable(true);
-	this.requestFocus();
-	snake = new SnakeObject();
-	this.addKeyListener(snake);
-		timer = new Timer(100, new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				snake.move(); // This method will handle the snake's movement
-				repaint(); // Repaint the game to reflect the movement
-			}
-		});
-		timer.start();
-  }
-  
-	public void paint(Graphics brush) {
-    	
-    	// sample code for printing message for debugging
-    	// counter is incremented and this message printed
-    	// each time the canvas is repainted
-		drawCheckeredBackground(brush);
-    	counter++;
-		brush.setColor(Color.BLUE);
-    	brush.drawString("Counter is " + counter,10,10);
-		brush.setColor(Color.GREEN);
-		snake.paint(brush);
+    private void drawCheckeredBackground(Graphics g) {
+        boolean isBlack = false;
+        int width = getWidth();
+        int height = getHeight();
+        int squareSize = 25;
 
-  }
+        for (int y = 0; y < height; y += squareSize) {
+            isBlack = !isBlack;
 
-	private void drawCheckeredBackground(Graphics g) {
-		boolean isBlack = false;
-		int width = getWidth();
-		int height = getHeight();
-		int squareSize = 25;
+            for (int x = 0; x < width; x += squareSize) {
+                if (isBlack) {
+                    g.setColor(Color.black);
+                } else {
+                    g.setColor(Color.WHITE);
+                }
+                g.fillRect(x, y, squareSize, squareSize);
 
-		for (int y = 0; y < height; y += squareSize) {
-			isBlack = !isBlack;
+                isBlack = !isBlack;
+            }
+        }
+    }
 
-			for (int x = 0; x < width; x += squareSize) {
-				if (isBlack) {
-					g.setColor(Color.black);
-				} else {
-					g.setColor(Color.WHITE);
-				}
-				g.fillRect(x, y, squareSize, squareSize);
+    public void update(Graphics brush) {
+        if (!snake.isGameOver()) {
+            super.update(brush);
+        } else {
+            timer.stop();
+        }
+    }
 
-				isBlack = !isBlack;
-			}
-		}
-	}
-
-	public void update(Graphics brush) {
-		if (!snake.isGameOver()) {
-			super.update(brush);
-		} else {
-			timer.stop();
-		}
-
-	}
-
-  
-	public static void main (String[] args) {
-   		Snake a = new Snake();
-		a.repaint();
-  }
-
-
+    public static void main(String[] args) {
+        Snake a = new Snake();
+        a.repaint();
+    }
 }
--- src/game/SnakeObject.java
+++ src/game/SnakeObject.java
@@ -5,13 +5,11 @@ import java.awt.event.*;
 import java.util.ArrayList;
 
 public class SnakeObject extends Polygon implements KeyListener {
-    static Point[] spawnPoints = {new Point(0, 0), new Point(0,
+    final static Point[] spawnPoints = {new Point(0, 0), new Point(0,
             25),
             new Point(25, 25), new Point(25, 0)};
-
     private ArrayList<SnakeSegment> snakeSegments = new ArrayList<>();
     private ArrayList<Point> positionsHistory = new ArrayList<>();
-
     private int direction = KeyEvent.VK_RIGHT;
     private Point[] snakePoints;
 
@@ -21,43 +19,25 @@ public class SnakeObject extends Polygon implements KeyListener {
         snakeSegments.add(new SnakeSegment(new Point(250, 250)));
         snakeSegments.add(new SnakeSegment(new Point(235, 250))); // Middle
         snakeSegments.add(new SnakeSegment(new Point(220, 250))); // Tail
-
     }
 
     public void move() {
         Point previousPosition = snakeSegments.get(0).getPosition();
-
-        // Move the head based on the current direction.
         snakeSegments.get(0).setPosition(getNextPosition());
 
-        // Then, for each of the body segments...
         for (int i = 1; i < snakeSegments.size(); i++) {
-            // Store the current position of this segment.
             Point currentPosition = snakeSegments.get(i).getPosition();
-
-            // Move this segment to the previous position of the segment in front of it.
             snakeSegments.get(i).setPosition(previousPosition);
-
-            // Update previousPosition for the next segment to use.
             previousPosition = currentPosition;
         }
     }
 
-
     public void paint(Graphics brush) {
-
         brush.setColor(Color.GREEN);
-
-        // Iterate through each segment of the snake
         for (SnakeSegment segment : snakeSegments) {
             segment.paint(brush);
         }
     }
-
-
-
-
-
 
     public boolean isGameOver() {
         if (snakePoints[0].x < 0 || snakePoints[0].x > 500
@@ -70,62 +50,65 @@ public class SnakeObject extends Polygon implements KeyListener {
     private Point getNextPosition() {
         Point headPosition = snakeSegments.get(0).getPosition();
         switch (direction) {
-            case KeyEvent.VK_UP: return new Point(headPosition.x, headPosition.y - 25);
-            case KeyEvent.VK_DOWN: return new Point(headPosition.x, headPosition.y + 25);
-            case KeyEvent.VK_LEFT: return new Point(headPosition.x - 25, headPosition.y);
-            case KeyEvent.VK_RIGHT: return new Point(headPosition.x + 25, headPosition.y);
-            default: return headPosition; // If no direction is pressed, don't move
+            case KeyEvent.VK_UP:
+                return new Point(headPosition.x, headPosition.y - 25);
+            case KeyEvent.VK_DOWN:
+                return new Point(headPosition.x, headPosition.y + 25);
+            case KeyEvent.VK_LEFT:
+                return new Point(headPosition.x - 25, headPosition.y);
+            case KeyEvent.VK_RIGHT:
+                return new Point(headPosition.x + 25, headPosition.y);
+            default:
+                return headPosition;
         }
     }
 
-
-        @Override
-        public void keyTyped (KeyEvent e){
-        }
-        @Override
-        public void keyReleased (KeyEvent e){
-        }
-
-        public void keyPressed (KeyEvent e){
-            int key = e.getKeyCode();
-
-            if ((key == KeyEvent.VK_RIGHT) && (direction != KeyEvent.VK_LEFT)) {
-                direction = KeyEvent.VK_RIGHT;
-            } else if ((key == KeyEvent.VK_LEFT) && (direction != KeyEvent.VK_RIGHT)) {
-                direction = KeyEvent.VK_LEFT;
-            } else if ((key == KeyEvent.VK_UP) && (direction != KeyEvent.VK_DOWN)) {
-                direction = KeyEvent.VK_UP;
-            } else if ((key == KeyEvent.VK_DOWN) && (direction != KeyEvent.VK_UP)) {
-                direction = KeyEvent.VK_DOWN;
-            }
-        }
-
-        public class SnakeSegment {
-            private Point position;
-
-            public SnakeSegment(Point position) {
-                this.position = position;
-
-            }
-            private void translate(int x, int y) {
-                position.x = position.x + x;
-                position.y = position.y + y;
-            }
-
-            public void paint(Graphics brush) {
-                brush.setColor(Color.GREEN);
-                brush.fillRect((int) position.x, (int) position.y, 25, 25);
-            }
-
-
-            public Point getPosition() {
-                return position;
-            }
-
-            public void setPosition(Point position) {
-                this.position = position;
-            }
-        }
-
-
+    @Override
+    public void keyTyped(KeyEvent e) {
     }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+
+        if ((key == KeyEvent.VK_RIGHT) && (direction != KeyEvent.VK_LEFT)) {
+            direction = KeyEvent.VK_RIGHT;
+        } else if ((key == KeyEvent.VK_LEFT) && (direction != KeyEvent.VK_RIGHT)) {
+            direction = KeyEvent.VK_LEFT;
+        } else if ((key == KeyEvent.VK_UP) && (direction != KeyEvent.VK_DOWN)) {
+            direction = KeyEvent.VK_UP;
+        } else if ((key == KeyEvent.VK_DOWN) && (direction != KeyEvent.VK_UP)) {
+            direction = KeyEvent.VK_DOWN;
+        }
+    }
+
+    public class SnakeSegment {
+        private Point position;
+
+        public SnakeSegment(Point position) {
+            this.position = position;
+
+        }
+
+        private void translate(int x, int y) {
+            position.x = position.x + x;
+            position.y = position.y + y;
+        }
+
+        public void paint(Graphics brush) {
+            brush.setColor(Color.GREEN);
+            brush.fillRect((int) position.x, (int) position.y, 25, 25);
+        }
+
+        public Point getPosition() {
+            return position;
+        }
+
+        public void setPosition(Point position) {
+            this.position = position;
+        }
+    }
+}
