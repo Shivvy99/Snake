f44766f27c9d7b064888d442b84ae99ce32ead1f
Shiven Khanna <Shivenkhanna05@gmail.com>
2024-03-03 23:03:01 -0500

    Added Comments to Interace
    Lets gooo we finished!

--- src/game/Consumables.java
+++ src/game/Consumables.java
@@ -3,11 +3,18 @@ package game;
  * Shyam Ganapathy, Shiven Khanna
  */
 import java.awt.*;
-
+/**
+ * This interface is used by both the Fruit and Powerup Class. It contains two methods for which the Fruit and Powerup
+ * classes need in order to function properly.
+ */
 public interface Consumables {
-
+    /**
+     * This method signature outlines the paint method in both the Fruit and Powerups classes.
+     */
     public void paint(Graphics brush);
-
+    /**
+     * This method signature outlines the calculateSpawnPoint method in both the Fruit and Powerups classes.
+     */
     Point calculateSpawnPoint();
 
 
